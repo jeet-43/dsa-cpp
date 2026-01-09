35b2d8a42833c77d268e66f19839654680b1b25b
Jeet Makhija <jeetmakhija3@gmail.com>
2026-01-09 22:15:34 +0530

    Added solution for First Missing Positive

---
+++ Arrays/012_first_missing_positive.cpp
@@ -0,0 +1,24 @@
+// Problem: first missing positive
+// Approach: Cyclic Sort
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Problem Link:https:https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/smallest-positive-missing-number-1587115621
+              //https://leetcode.com/problems/first-missing-positive/description/
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n &&
+                   nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+
+        return n + 1;
+    }
+};
