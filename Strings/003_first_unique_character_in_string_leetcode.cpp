97834a7809caf6631268c834a435e73bae4616cb
Jeet Makhija <jeetmakhija3@gmail.com>
2026-01-13 23:03:51 +0530

    Added solution for First Unique Character in String

---
+++ Strings/003_first_unique_character_in_string_leetcode.cpp
@@ -0,0 +1,35 @@
+// Problem: first non repeating character
+// Approach: by storing indices (one traversal)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Problem Link:https://leetcode.com/problems/first-unique-character-in-a-string/description/
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        const int MAX_CHAR = 26;
+        vector<int> vis(MAX_CHAR, -1);
+
+        for (int i = 0; i < s.length(); i++) {
+            int index = s[i] - 'a';
+            if (vis[index] == -1) {
+                vis[index] = i;
+            } else {
+                vis[index] = -2;
+            }
+        }
+
+        int idx = -1;
+        for (int i = 0; i < MAX_CHAR; i++) {
+            if (vis[i] >= 0) {
+                if (idx == -1 || vis[i] < vis[idx]) {
+                    idx = i;
+                }
+            }
+        }
+
+        if (idx == -1)
+            return -1;
+
+        return vis[idx];
+    }
+};
