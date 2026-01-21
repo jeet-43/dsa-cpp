8fa15cc997b3200e9d7207a613a789704052a71a
Jeet Makhija <jeetmakhija3@gmail.com>
2026-01-21 19:56:16 +0530

    Added solution for Search in Rotated Array

--- Searching/005_search_in_rotated_array.cpp
+++ Searching/005_search_in_rotated_array.cpp
@@ -2,7 +2,7 @@
 // Approach: using binary search
 // Time Complexity: O(log n)
 // Space Complexity: O(1)
-// Problem Link:https://www.geeksforgeeks.org/batch/gfg-160-problems/track/searching-gfg-160/problem/search-in-a-rotated-array4618
+// Problem Link: https://www.geeksforgeeks.org/batch/gfg-160-problems/track/searching-gfg-160/problem/search-in-a-rotated-array4618
              // https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 
 class Solution {
