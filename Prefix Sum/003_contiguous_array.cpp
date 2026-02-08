fb7ffaaaf938651d0288c8515e828495cf76288f
Jeet Makhija <jeetmakhija3@gmail.com>
2026-02-08 17:42:30 +0530

    Added solution for Contiguous Array

---
+++ Prefix Sum/003_contiguous_array.cpp
@@ -0,0 +1,24 @@
+// Problem: Contiguous Array
+// Approach: Prefix Sum + Hashing
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// Problem Link:https://leetcode.com/problems/contiguous-array/submissions/1912394679/
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+
+        int sum = 0, ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += (nums[i] == 0 ? -1 : 1);
+            if(mp.count(sum))
+                ans = max(ans, i - mp[sum]);
+            else
+                mp[sum] = i;
+        }
+        return ans;
+    }
+};
+
