16f02d9b07098a5421ee0a52c8740cf8dcca548a
Jeet Makhija <jeetmakhija3@gmail.com>
2026-02-15 14:53:33 +0530

    Added solution for Valid Parentheses

---
+++ Stack/001_valid_parentheses.cpp
@@ -0,0 +1,30 @@
+// Problem: Valid Parentheses
+// Approach: Stack
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// Problem Link:https://leetcode.com/problems/valid-parentheses/description/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
