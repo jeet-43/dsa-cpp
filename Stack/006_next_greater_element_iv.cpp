1087dfc0ca9dec611c5da7acd56de023216a480f
Jeet Makhija <jeetmakhija3@gmail.com>
2026-02-18 08:54:38 +0530

    Added solution for Next Greater Element IV

---
+++ Stack/006_next_greater_element_iv.cpp
@@ -0,0 +1,42 @@
+// Problem: Next Greater Element IV
+// Approach: NSL + NSR
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// Problem Link:https://leetcode.com/problems/next-greater-element-iv/description/
+
+class Solution {
+public:
+    vector<int> maxOfMin(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        vector<int> ans(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            int len = right[i] - left[i] - 1;
+            ans[len] = max(ans[len], arr[i]);
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            ans[i] = max(ans[i], ans[i + 1]);
+        }
+
+        ans.erase(ans.begin()); 
+        return ans;
+    }
+};
