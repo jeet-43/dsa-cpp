c9b161acc135160d27b3f2adfc5525da7328e104
Jeet Makhija <jeetmakhija3@gmail.com>
2026-02-22 14:53:48 +0530

    Added solution for Longest Consecutive Sequence

---
+++ Hashing/003_longest_consecutive_sequence.cpp
@@ -0,0 +1,29 @@
+// Problem: Longest Consecutive Sequence
+// Approach: HashSet
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// Problem Link:https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int num : st) {
+            if (st.find(num - 1) == st.end()) {
+                int current = num;
+                int length = 1;
+
+                while (st.find(current + 1) != st.end()) {
+                    current++;
+                    length++;
+                }
+
+                longest = max(longest, length);
+            }
+        }
+
+        return longest;
+    }
+};
