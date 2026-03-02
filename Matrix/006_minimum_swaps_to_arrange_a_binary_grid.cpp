5947b1bb8994ac1f8dc10b4656a608ebcd5856fb
Jeet Makhija <jeetmakhija3@gmail.com>
2026-03-02 19:34:29 +0530

    Added solution for Minimum Swaps to Arrange a Binary Grid

---
+++ Matrix/006_minimum_swaps_to_arrange_a_binary_grid.cpp
@@ -0,0 +1,40 @@
+// Problem: Minimum Swaps to Arrange a Binary Grid (Daily Challenge -MAR 2)
+// Approach: Greedy
+// Time Complexity: O(n²)
+// Space Complexity: O(n)
+// Problem Link:https://leetcode.com/problems/minimum-swaps-to-arrange-a-binary-grid/?envType=daily-question&envId=2026-03-02
+
+class Solution {
+public:
+    int minSwaps(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> pos(n, -1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (grid[i][j] == 1) {
+                    pos[i] = j;
+                    break;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int k = -1;
+            for (int j = i; j < n; ++j) {
+                if (pos[j] <= i) {
+                    ans += j - i;
+                    k = j;
+                    break;
+                }
+            }
+            if (~k) {
+                for (int j = k; j > i; --j) {
+                    swap(pos[j], pos[j - 1]);
+                }
+            } else {
+                return -1;
+            }
+        }
+        return ans;
+    }
+};
