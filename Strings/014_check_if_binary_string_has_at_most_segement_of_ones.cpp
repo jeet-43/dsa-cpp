820496eb4bcf48dc1b588e402e16c658dc151a88
Jeet Makhija <jeetmakhija3@gmail.com>
2026-03-06 19:09:07 +0530

    Added solution for Check if Binary String Has at Most One Segement of Ones

---
+++ Strings/014_check_if_binary_string_has_at_most_segement_of_ones.cpp
@@ -0,0 +1,20 @@
+// Problem: Check if Binary String Has at Most One Segement of Ones (Daily Challenge -MAR 6)
+// Approach: Scan
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Problem Link:https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/?envType=daily-question&envId=2026-03-05
+
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        int count = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1' && (i == 0 || s[i-1] == '0')) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+};
