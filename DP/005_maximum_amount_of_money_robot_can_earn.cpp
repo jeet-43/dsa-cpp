6f1725febfc90354f83e54d9944c9c5f2ceaab90
Jeet Makhija <jeetmakhija3@gmail.com>
2026-04-02 22:25:28 +0530

    Add Solution for Maximum Amount of Money Robot Can Earn

---
+++ DP/005_maximum_amount_of_money_robot_can_earn.cpp
@@ -0,0 +1,25 @@
+// Problem: Maximum Amount of Money Robot Can Earn (Daily Challenge -MAR 23)
+// Approach: DP
+// Time Complexity: O(nm)
+// Space Complexity: O(nm)
+// Problem Link:https://leetcode.com/problems/maximum-amount-of-money-robot-can-earn/description/?envType=daily-question&envId=2026-04-02
+
+class Solution {
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+        int n = coins.size(), m = coins[0].size();
+        vector dp(n, vector(m, vector<int>(3, -1e9)));
+        dp[0][0][1] = dp[0][0][2] = 0, dp[0][0][0] = coins[0][0];
+        
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                for (int k = 0; k < 3; k++) {
+                    if (i) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k] + coins[i][j]);
+                    if (i && k) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k - 1]);
+                    if (j) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k] + coins[i][j]);
+                    if (j && k) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k - 1]);
+                }
+        int ans = *max_element(dp[n - 1][m - 1].begin(), dp[n - 1][m - 1].end());
+        return ans;
+    }
+};
